2f03d2efa6a7aa9e6915dbcb0f2b99babca72ee1
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-09-05 13:32:28 +0200

    lunch time

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,27 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @porfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to @portfolio, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+    end
 end
