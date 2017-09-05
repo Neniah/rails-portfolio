e084d953fc2a2351c9e41bd0fe9d2e8fe9a86858
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-09-05 12:01:24 +0200

    adding from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "Title #{blog}",
+    body: "Contrary to popular belief,
+    Lorem Ipsum is not simply random text. It has
+    roots in a piece of classical Latin literature
+    from 45 BC, making it over 2000 years old.
+    Richard McClintock, a Latin professor at
+    Hampden-Sydney College in Virginia, looked up
+    one of the more obscure Latin words, consectetur,
+    from a Lorem Ipsum passage, and going through
+    the cites of the word in classical literature, discovered the
+    undoubtable source."
+  )
+end
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+9.times do |item|
+  Portfolio.create!(
+    title: "Portfolio title #{item}",
+    subtitle: "The subtitle is good.",
+    body: "Contrary to popular belief,
+    Lorem Ipsum is not simply random text. It has
+    roots in a piece of classical Latin literature
+    from 45 BC, making it over 2000 years old.",
+    main_image: 'http://via.placeholder.com/600x400',
+    thumb_image: 'http://via.placeholder.com/350x200'
+  )
+end
+
+puts "adding 9 portfolio items"
+puts "adding 5 skills"
+puts "adding 10 blogs posts"
