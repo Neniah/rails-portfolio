ba43a4bc60e7131fc1e99eb43c9ba89950aa4f24
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-09-12 18:50:43 +0200

    Topics and blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910154547) do
+ActiveRecord::Schema.define(version: 20170912163611) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170910154547) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.integer "topic_id"
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "portfolios", force: :cascade do |t|
@@ -33,6 +35,12 @@ ActiveRecord::Schema.define(version: 20170910154547) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
