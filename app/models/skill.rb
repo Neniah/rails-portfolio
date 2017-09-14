1d5a525ac1dd168fd1b7ddc6c6a37fd1889a79c8
Maria Lobillo Santos <maria@Marias-MacBook-Pro.local>
2017-09-14 13:22:42 +0200

    set defaults for skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
   validates_presence_of :title, :percent_utilized
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
